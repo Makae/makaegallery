7ac905b9be59c491d0d245bad045ff24f5897bc9
kasm <martin.kaeser@zuehlke.com>
2021-10-24 18:19:58 +0200

    Improve gallery rendering move to simple columns

--- frontend/src/app/components/gallery/gallery-masonry/gallery-masonry.component.ts
+++ frontend/src/app/components/gallery/gallery-masonry/gallery-masonry.component.ts
@@ -2,11 +2,17 @@ import {Component, OnInit} from '@angular/core';
 import {GalleryService} from '../../../shared/services/gallery.service';
 import {ActivatedRoute} from '@angular/router';
 import {filter, map, switchMap} from 'rxjs/operators';
-import {Dimensions, Image} from '../../../shared/models/gallery-model';
+import {Image} from '../../../shared/models/gallery-model';
 import {CapabilityService} from '../../../shared/services/capability.service';
 
 interface DisplayedImage extends Image {
   cssClass: string;
+}
+
+interface DisplayedColumn {
+  images: DisplayedImage[];
+  imageCount: number;
+  heightSum: number;
 }
 
 @Component({
@@ -15,9 +21,10 @@ interface DisplayedImage extends Image {
   styleUrls: ['./gallery-masonry.component.scss']
 })
 export class GalleryMasonryComponent implements OnInit {
-  public images?: DisplayedImage[];
+  private static readonly NUM_COLUMNS = 3;
   public containerWidth?: number;
   public width = 250;
+  public columns: DisplayedColumn[] = [];
 
   public constructor(
     public galleryService: GalleryService,
@@ -53,15 +60,38 @@ export class GalleryMasonryComponent implements OnInit {
   }
 
   public ngOnInit(): void {
+    this.columns = [];
+    for (let i = 0; i < GalleryMasonryComponent.NUM_COLUMNS; i++) {
+      this.columns.push({imageCount: 0, heightSum: 0, images: []})
+    }
+
     this.activatedRoute.params.pipe(
       map(params => params['galleryId'] as string),
       filter(galleryId => !!galleryId),
       switchMap(galleryId => this.galleryService.getGallery(galleryId)),
-      map(gallery => gallery.images),
-      // map(images => images.map(GalleryMasonryComponent.mapImageToMasonryImage))
-      map(images => images?.map(GalleryMasonryComponent.mapImageToDisplayedImage))
+      map(gallery => gallery.images as Image[]),
+      map(images => images.map(GalleryMasonryComponent.mapImageToDisplayedImage))
     ).subscribe(images => {
-      this.images = images;
+      this.assignImagesToColumns(images);
     });
+  }
+
+  private assignImagesToColumns(images: DisplayedImage[]) {
+    for (let image of images) {
+      const columnIndex = GalleryMasonryComponent.findNextColumnIndex(this.columns);
+      const column = this.columns[columnIndex];
+      column.images.push(image);
+      column.heightSum += image.dimensions.height
+    }
+  }
+
+  private static findNextColumnIndex(columns: DisplayedColumn[]): number {
+    let index = -1;
+    for (let i = 0; i < columns.length; i++) {
+      if(index === -1 || columns[i].heightSum < columns[index].heightSum) {
+        index = i;
+      }
+    }
+    return index;
   }
 }
